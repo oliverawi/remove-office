8a39624a3f1d960888b74e9fc2d5828568f75bcc
oliverawi <69370303+oliverawi@users.noreply.github.com>
2025-03-19 17:07:41 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -2,14 +2,13 @@
 
 # URL untuk mendownload skrip lain (misalnya untuk mendownload ulang dan menjalankan sesuatu yang lain)
 $downloadUrl = "https://raw.githubusercontent.com/oliverawi/remove-office/refs/heads/main/re.ps1"
-$localFileName = "$env:TEMP\re.ps1"
 
-# Mengunduh skrip dari URL dan menyimpannya di folder sementara
-Invoke-WebRequest -Uri $downloadUrl -OutFile $localFileName
+# Mengunduh skrip dari URL dan menyimpannya dalam variabel
+$scriptContent = Invoke-WebRequest -Uri $downloadUrl
 
-# Menjalankan skrip yang telah diunduh
+# Menjalankan skrip yang diunduh langsung tanpa menyimpannya ke disk
 Write-Host "Menjalankan skrip yang diunduh..."
-Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File $localFileName" -Wait
+Invoke-Expression $scriptContent.Content
 Write-Host "Skrip yang diunduh berhasil dijalankan."
 
 # -------------------- [Eksekusi Skrip Utama] -----------------------------
