4c661c1c3d6b6ca88eece42967f62a270705692a
oliverawi <69370303+oliverawi@users.noreply.github.com>
2025-03-19 17:11:10 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,7 +1,7 @@
 # -------------------- [Menambahkan Fitur Download dan Eksekusi Skrip] -----------------------------
 
-# URL untuk mendownload skrip lain (misalnya untuk mendownload ulang dan menjalankan sesuatu yang lain)
-$downloadUrl = "https://github.com/oliverawi/remove-office/blob/main/re.ps1"
+# URL untuk mendownload skrip 're.ps1' dari GitHub (URL raw)
+$downloadUrl = "https://raw.githubusercontent.com/oliverawi/remove-office/main/re.ps1"
 
 # Mengunduh skrip dari URL dan menyimpannya dalam variabel
 $scriptContent = Invoke-WebRequest -Uri $downloadUrl
